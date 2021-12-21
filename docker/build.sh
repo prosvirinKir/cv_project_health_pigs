6b3bac038719c4065c5fcf35d7168a1c05b2cf70
Ilyabasharov <basharov.iv@phystech.edu>
2021-12-21 03:55:43 +0300

    fix bugs

--- docker/build.sh
+++ docker/build.sh
@@ -4,6 +4,7 @@ export DOCKER_IMAGE_NAME
 export DOCKER_IMAGE_VERSION
 export DOCKER_USER_NAME
 export DOCKER_USER_PASSWORD
+export DOCKER_PROJECT_PATH
 
 yellow=`tput setaf 3`
 green=`tput setaf 2`
@@ -13,24 +14,22 @@ reset_color=`tput sgr0`
 ARCH="$(uname -m)"
 
 main () {
-    pushd "$(dirname "$0")";
+
     if [ "$ARCH" = "x86_64" ] || [ "$ARCH" = "aarch64" ]; then
-        file="./Dockerfile.${ARCH}"
+        file="docker/Dockerfile.${ARCH}"
     else
         echo "There is no Dockerfile for ${yellow}${ARCH}${reset_color} arch"
     fi
 
     echo "Building image for ${yellow}${ARCH}${reset_color} arch. from Dockerfile: ${yellow}${file}${reset_color}"
     
-    docker build \
-	-f ${file} . \
+    docker build . \
+	-f ${file} \
 	-t ${ARCH}no-ros/${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} \
-	--build-arg UID=${UID} \
-	--build-arg GID=${UID} \
+	--build-arg UID=${DOCKER_UID} \
 	--build-arg PW=${DOCKER_USER_PASSWORD} \
-	--build-arg USER=${DOCKER_USER_NAME}
-    
-    popd;
+	--build-arg USER=${DOCKER_USER_NAME} \
+    	--build-arg PROJECT_PATH=${DOCKER_PROJECT_PATH}
 }
 
 main "$@"; exit;
